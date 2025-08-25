34692e98a06a6122cbc00db51b1243cdd3674dcd
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-08-25 11:54:32 +0530

    Create Patterns3.java

---
+++ 25-8-2025/Patterns3.java
@@ -0,0 +1,30 @@
+/*
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+*/
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the value: ");
+        int n = s.nextInt();
+        for(int i=n;i>=1;i--){
+            
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(" ");
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print("* ");
+            }
+            if(i!=1) System.out.println();
+        }
+        
+        
+	}
+}
