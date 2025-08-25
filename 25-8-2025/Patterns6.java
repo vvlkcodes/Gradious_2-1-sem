9a6bc11b9a78ee80a1b0869126a962527accfae4
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-08-25 12:47:04 +0530

    Create Patterns6.java

---
+++ 25-8-2025/Patterns6.java
@@ -0,0 +1,32 @@
+/*
+     a
+    aba
+   abcba
+  abcdcba
+ abcdedcba
+*/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the value: ");
+        int n = s.nextInt();
+        for(int i=1;i<=n;i++){
+            //Spaces
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            // ab ->
+            for(int j=97;j<97+i;j++){
+                System.out.print((char)j);
+            }
+            // ba<-
+            for(int j=97+i-2;j>=97;j--){
+                System.out.print((char)j);
+            }
+            System.out.println();
+        }
+        
+	}
+}
