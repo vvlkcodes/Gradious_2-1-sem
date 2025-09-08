000a3ca47bce5f5081043b90c2b705befcd100f5
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-09-08 14:04:05 +0530

    Create RemoveDuplicates.java

---
+++ 8-9-2025/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+// remove the duplicates of the given array and print it's numbers in it's own order.
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr = {6,7,7,5,6,3,4,2,3,5,2,1,1,1,2};
+	    for(int i=0;i<arr.length;i++){
+	        for(int j=i+1;j<arr.length;j++){
+	            if(arr[i] == arr[j]) arr[j]=-1;
+	        }
+	    }
+	    System.out.println(Arrays.toString(arr));
+	    for(int i=0;i<arr.length;i++){
+	        if(arr[i] != -1) System.out.print(" "+arr[i]);
+	    }
+	}
+}
