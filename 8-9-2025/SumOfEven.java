93b26b23e3bf36343e9126a0d75936fe752a71f6
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-09-08 10:24:17 +0530

    Create SumOfEven.java

---
+++ 8-9-2025/SumOfEven.java
@@ -0,0 +1,22 @@
+// print the sum of even numbers
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	    System.out.println("Enter the number of elements: ");
+	    int n = s.nextInt();
+	    int[] arr = new int[n];
+	    System.out.println("Enter the elements of array: ");
+	    for(int i=0;i<n;i++){
+	        arr[i] = s.nextInt();
+	    }
+	    int sum = 0;
+	    for(int i=0;i<n;i++){
+	        if(arr[i] % 2 == 0)sum += arr[i];
+	    }
+	    System.out.println("The sum of the even elements of the array is: ");
+	    System.out.println(sum);
+	}
+}
