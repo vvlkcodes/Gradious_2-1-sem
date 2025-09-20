cf90552ee062d8ae423194d3632412c4ecc29d13
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-09-20 11:27:16 +0530

    Create TransposeBySwap.java

---
+++ 20-9-2025/TransposeBySwap.java
@@ -0,0 +1,36 @@
+// 2D arrays
+// Print the transpose of the given matrix by Swapping
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number of rows: ");
+		int n = s.nextInt();
+		System.out.println("Enter the number of columns: ");
+		int m = s.nextInt();
+		int[][] arr = new int[n][m];
+        System.out.println("Enter the elements: ");
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				arr[i][j] = s.nextInt();
+			}
+		}
+        System.out.println("The elements you entered are: ");
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<i; j++) {
+				int t =  arr[i][j];
+				 arr[i][j] =  arr[j][i];
+				arr[j][i] = t;
+			}
+			System.out.println();
+		}
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println(Arrays.deepToString(arr));
+	}
+}
