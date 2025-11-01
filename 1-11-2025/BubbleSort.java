103505ace790367c99595cfe35fd8c2ae53bd32f
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-11-01 15:46:00 +0530

    Update BubbleSort.java

--- 1-11-2025/BubbleSort.java
+++ 1-11-2025/BubbleSort.java
@@ -5,7 +5,7 @@ public class Main{
     public static void main(String[] args){
         int[] arr = {34,65,45,76,12};
         System.out.println(Arrays.toString(arr));
-        int f = 1;
+        int f = 0;
         for(int i=0;i<arr.length-1;i++){
             for(int j=1;j<arr.length;j++){
                 if(arr[i] > arr[j]){
