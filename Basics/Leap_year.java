74b513fa4377ba507a17de1b46877a8b5d3a70d5
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-11-20 19:17:23 +0530

    Update Leap_year.java

--- Basics/Leap_year.java
+++ Basics/Leap_year.java
@@ -2,16 +2,17 @@
 // is 1986 a leap year or not
 // Century years are never the leap years except the Century years that occurs once after 400 years.
 
-import java.util.Scanner;
 
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter the year: ");
-		int n = s.nextInt();
-		if(n%4==0 && n%400==0 || n%100!=0) System.out.println("Leap");
-		else System.out.println("Not a Leap Year");
-		System.out.println(1986/400);
-	}
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the year: ");
+        int year = s.nextInt();
+        if(year%400 == 0 || (year%4 == 0 && year%100 != 0)){
+            System.out.println("Leap");
+        }else{
+            System.out.println("No");
+        }
+    }
 }
